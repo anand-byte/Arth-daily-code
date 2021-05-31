e6f8845c4590c93c48d3410cf54bec2acef357df
Abhishek <anandabhishek090@gmail.com>
2021-05-31 23:47:38 +0530

    GrossSalary from base salary and some allowance

---
+++ ArthQ1.cpp
@@ -0,0 +1,12 @@
+/*Ramesh's basic salary is input through the keyboard. His dearness allowance is 40% of basic salary, and house rent allowance is 20% of basic salary. Write a program to calculate his gross salary.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  float basic_sal;
+  cout<<"Enter your basic Salary: ";
+  cin>>basic_sal;
+  cout<<"\nyour Gross Salary is :Rs "<<((float)basic_sal+(float)basic_sal*(0.4)+(float)basic_sal*(0.2));
+} 
